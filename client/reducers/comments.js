785ad30fb5bcf1ee745c398967b7895d475f95e5
danweinstock <dan.weinstock@yahoo.com>
2017-06-27 15:37:47 -0400

    added Redux reducers and corrected syntax on one element to align with ES6

---
+++ client/reducers/comments.js
@@ -0,0 +1,12 @@
+//reducer takes in 2 things
+
+//1. the acton (info about what happened)
+
+//2. a copy of current state
+
+function comments(state = []){
+	console.log(state, action);
+	return state;
+}
+
+export default comments
---
+++ client/reducers/posts.js
@@ -0,0 +1,12 @@
+//reducer takes in 2 things
+
+//1. the acton (info about what happened)
+
+//2. a copy of current state
+
+function posts(state = []){
+	console.log(state, action);
+	return state;
+}
+
+export default posts
