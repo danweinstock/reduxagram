c3899dc418b8caac6264fe871bd63e55c65a64b7
danweinstock <dan.weinstock@yahoo.com>
2017-06-27 14:51:54 -0400

    basic router setup completed with nested routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 import { render } from 'react-dom';
 
-import css from './styles/style.styl'
+// Import css
+import css from './styles/style.styl';
 
-//import components
+// Import Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
